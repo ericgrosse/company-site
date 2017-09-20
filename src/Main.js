3dce0d1dbf4420e992ab016740266398f7501681
ericgrosse <ericgrosse1@gmail.com>
2017-09-20 15:34:12 -0400

    first commit

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Roster from './components/Roster'
+import Schedule from './components/Schedule'
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/roster' component={Roster}/>
+      <Route path='/schedule' component={Schedule}/>
+    </Switch>
+  </main>
+);
+
+export default Main
