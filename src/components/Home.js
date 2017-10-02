355b4015de4acf23b4c9c543e4023488e1837d98
ericgrosse <ericgrosse1@gmail.com>
2017-10-02 16:28:46 -0400

    Home.js sample CSS Grid implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,53 @@ import './Home.scss'
 
 const Home = () => (
   <div className="Home">
-    <h1>Welcome to the Tornadoes Website!</h1>
+    <div className="container">
+      <div className="item item-1">
+        <p>Item 1</p>
+      </div>
+      <div className="item item-2">
+        <p>Item 2</p>
+      </div>
+      <div className="item item-3">
+        <p>Item 3</p>
+      </div>
+      <div className="item item-4">
+        <p>Item 4</p>
+      </div>
+      <div className="item item-5">
+        <p>Item 5</p>
+      </div>
+      <div className="item item-6">
+        <p>Item 6</p>
+      </div>
+      <div className="item item-7">
+        <p>Item 7</p>
+      </div>
+      <div className="item item-8">
+        <p>Item 8</p>
+      </div>
+      <div className="item item-9">
+        <p>Item 9</p>
+      </div>
+      <div className="item item-10">
+        <p>Item 10</p>
+      </div>
+      <div className="item item-11">
+        <p>Item 11</p>
+      </div>
+      <div className="item item-12">
+        <p>Item 12</p>
+      </div>
+      <div className="item item-13">
+        <p>Item 13</p>
+      </div>
+      <div className="item item-14">
+        <p>Item 14</p>
+      </div>
+      <div className="item item-15">
+        <p>Item 15</p>
+      </div>
+    </div>
   </div>
 )
 
