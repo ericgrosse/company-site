c07ec5582098c78e66d5ae12166b630754af4d6c
ericgrosse <ericgrosse1@gmail.com>
2017-10-04 16:35:52 -0400

    more grid action

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,123 @@
 import React, { Component } from 'react'
 import './Home.scss'
-import img from '../images/tattoo-1.jpg'
+import img1 from '../images/tattoo-1.jpg'
+import img2 from '../images/tattoo-2.jpg'
+import img3 from '../images/tattoo-3.jpg'
+import img4 from '../images/tattoo-4.jpg'
+import img5 from '../images/tattoo-5.jpg'
+import img6 from '../images/tattoo-6.jpg'
+import img7 from '../images/tattoo-7.jpg'
+import img8 from '../images/tattoo-8.jpg'
 
 const Home = () => (
   <div className="Home">
     <div className="container">
       <div className="item item-1">
-        <img src={img}/>
+
       </div>
+
       <div className="item item-2">
-        <p>Item 2</p>
+        <img src={img1}/>
       </div>
+      
       <div className="item item-3">
-        <p>Item 3</p>
+
       </div>
+      
       <div className="item item-4">
-        <p>Item 4</p>
+        <img src={img2}/>
       </div>
+      
       <div className="item item-5">
-        <p>Item 5</p>
+
       </div>
+      
       <div className="item item-6">
-        <p>Item 6</p>
+        <img src={img3}/>
       </div>
+      
       <div className="item item-7">
-        <p>Item 7</p>
+
       </div>
+      
       <div className="item item-8">
-        <p>Item 8</p>
+        <img src={img4}/>
+      </div>
+      
+      <div className="item item-9">
+        
+      </div>
+
+      <div className="item item-10">
+
+      </div>
+
+      <div className="item item-11">
+
+      </div>
+      
+      <div className="item item-12">
+
+      </div>
+      
+      <div className="item item-13">
+
+      </div>
+      
+      <div className="item item-14">
+
+      </div>
+      
+      <div className="item item-15">
+
+      </div>
+      
+      <div className="item item-16">
+
+      </div>
+      
+      <div className="item item-17">
+
+      </div>
+      
+      <div className="item item-18">
+        
+      </div>
+
+      <div className="item item-19">
+
+      </div>
+      
+      <div className="item item-20">
+        <img src={img5}/>
+      </div>
+      
+      <div className="item item-21">
+
+      </div>
+      
+      <div className="item item-22">
+        <img src={img6}/>
+      </div>
+      
+      <div className="item item-23">
+
+      </div>
+      
+      <div className="item item-24">
+        <img src={img7}/>
+      </div>
+      
+      <div className="item item-25">
+
+      </div>
+      
+      <div className="item item-26">
+        <img src={img8}/>
+      </div>
+      
+      <div className="item item-27">
+        
       </div>
     </div>
   </div>
