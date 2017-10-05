d8324a866709d33eec8123823e837e5c273f2d52
ericgrosse <ericgrosse1@gmail.com>
2017-10-04 23:12:36 -0400

    removed unused components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
-import Roster from './Roster'
-import Schedule from './Schedule'
 
 const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/roster' component={Roster}/>
-      <Route path='/schedule' component={Schedule}/>
-    </Switch>
-  </main>
+  <div className="Main">
+    <Home/>
+  </div>
 );
 
 export default Main
