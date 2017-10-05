b8b77ab7fbe6883c8dbe2a8f10615cb73b23d12c
ericgrosse <ericgrosse1@gmail.com>
2017-10-05 17:33:09 -0400

    added carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 import 'components/Home/Home.scss'
+import Slider from 'react-slick'
 
-const Home = () => (
-  <div className="Home">
-    <h1>Home</h1>
-  </div>
-)
+const Home = () => {
+  const settings = {
+    className: 'center',
+    dots: true,
+    centerMode: true,
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 500
+  }
+
+  return (
+    <div className="Home">
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div><img src={require('images/tattoo-1.jpg')}/></div>
+          <div><img src={require('images/tattoo-2.jpg')}/></div>
+          <div><img src={require('images/tattoo-3.jpg')}/></div>
+          <div><img src={require('images/tattoo-4.jpg')}/></div>
+          <div><img src={require('images/tattoo-5.jpg')}/></div>
+        </Slider>
+      </div>
+    </div>
+  )
+}
 
 export default Home
