1eacfc400db8757b90201bfca7afdd2e98b833e7
ericgrosse <ericgrosse1@gmail.com>
2017-10-06 08:07:01 -0400

    added another section to the homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,6 +24,33 @@ const Home = () => {
           <div><img src={require('images/tattoo-5.jpg')}/></div>
         </Slider>
       </div>
+
+      <div className="grid-container">
+        <div className="gutter"/>
+
+        <div className="item item-1">
+          <h3>Our Services</h3>
+          <img src={require('images/tattoo-1.jpg')}/>
+          <p>We offer web development services for projects of any scale</p>
+          <button>What we offer</button>
+        </div>
+
+        <div className="item item-2">
+          <h3>Our Portfolio</h3>
+          <img src={require('images/tattoo-2.jpg')}/>
+          <p>We build responsive websites with fluid layouts and elegant animations</p>
+          <button>What we've done</button>
+        </div>
+
+        <div className="item item-3">
+          <h3>About Us</h3>
+          <img src={require('images/tattoo-3.jpg')}/>
+          <p>We are a small team of dedicated web developers and concept artists</p>
+          <button>What we're about</button>
+        </div>
+
+        <div className="gutter"/>
+      </div>
     </div>
   )
 }
