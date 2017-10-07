474d7eaee9dc90af5fdedade77ee8c12418ee600
ericgrosse <ericgrosse1@gmail.com>
2017-10-07 16:27:49 -0400

    Services.js added a layout that extensively uses subgrids

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,69 @@ import 'components/Services.scss'
 
 const Services = () => (
   <div className="Services">
-    <h1>Services</h1>
+    <div className="wrapper">
+      <div className="one">
+        <div className="sub-1">
+          Sub 1
+        </div>
+        <div className="sub-2">
+          Sub 2
+        </div>
+      </div>
+      
+      <div className="two">
+        <div className="sub-1">
+          Sub 1
+        </div>
+
+        <div className="sub-2">
+          Sub 2
+        </div>
+
+        <div className="sub-3">
+          Sub 3
+        </div>
+      </div>
+      
+      <div className="three">
+        <div className="sub-1">
+          Sub 1
+        </div>
+
+        <div className="sub-2">
+          Sub 2
+        </div>
+        
+        <div className="sub-3">
+          Sub 3
+        </div>
+        
+        <div className="sub-4">
+          Sub 4
+        </div>
+        
+        <div className="sub-5">
+          Sub 5
+        </div>
+        
+        <div className="sub-6">
+          Sub 6
+        </div>
+      </div>
+      
+      <div className="four">Four</div>
+      <div className="five">
+        <div className="sub-1">
+          Sub 1
+        </div>
+
+        <div className="sub-2">
+          Sub 2
+        </div>
+      </div>
+      
+      <div className="six">Six</div>
+    </div>
   </div>
 )
 
