de5435d167f94ddf3e7632eb9ee894ab96b7b8cb
ericgrosse <ericgrosse1@gmail.com>
2017-10-20 09:35:39 -0400

    grid is so awesome

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,55 @@ import 'components/About.scss'
 const About = () => (
   <div className="About">
     <div className="wrapper">
-      <div className="section one">One</div>
-      <div className="section two">Two</div>
-      <div className="section three">Three</div>
-      <div className="section four">Four</div>
-      <div className="section five">Five</div>
-      <div className="section six">Six</div>
+      <div className="section one">
+        <div className="subsection photo">
+          Photo
+        </div>
+
+        <div className="subsection content">
+          Content
+        </div>
+      </div>
+
+      <div className="section two">
+        <div className="subsection photo">
+          Photo
+        </div>
+
+        <div className="subsection content">
+          Content
+        </div>
+      </div>
+      
+      <div className="section three">
+        <div className="subsection photo">
+          Photo
+        </div>
+
+        <div className="subsection content">
+          Content
+        </div>
+      </div>
+      
+      <div className="section four">
+        <div className="subsection photo">
+          Photo
+        </div>
+
+        <div className="subsection content">
+          Content
+        </div>
+      </div>
+      
+      <div className="section five">
+        <div className="subsection photo">
+          Photo
+        </div>
+
+        <div className="subsection content">
+          Content
+        </div>
+      </div>
     </div>
   </div>
 )
