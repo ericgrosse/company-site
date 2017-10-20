d2f2759baa4e6760dc4ad56330e3358477c79f4c
ericgrosse <ericgrosse1@gmail.com>
2017-10-20 09:21:49 -0400

    About page layout setup

--- src/components/About.js
+++ src/components/About.js
@@ -5,23 +5,12 @@ import 'components/About.scss'
 const About = () => (
   <div className="About">
     <div className="wrapper">
-      <div className="one">One</div>
-      <div className="two">Two</div>
-      <div className="three">Three</div>
-      <div className="four">Four</div>
-      <div className="five">Five</div>
-      <div className="six">Six</div>
-      <div className="seven">Seven</div>
-      
-      <div className="eight">
-        <div className="sub-1">Sub 1</div>
-        <div className="sub-2">Sub 2</div>
-      </div>
-      
-      <div className="nine">Nine</div>
-      <div className="ten">Ten</div>
-      <div className="eleven">Eleven</div>
-      <div className="twelve">Twelve</div>
+      <div className="section one">One</div>
+      <div className="section two">Two</div>
+      <div className="section three">Three</div>
+      <div className="section four">Four</div>
+      <div className="section five">Five</div>
+      <div className="section six">Six</div>
     </div>
   </div>
 )
