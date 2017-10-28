8a979b3d547050601c655c1db52019cbf209ad81
ericgrosse <ericgrosse1@gmail.com>
2017-10-28 11:46:54 -0400

    Portfolio page updated layout

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,79 +4,82 @@ import 'components/Portfolio.scss'
 
 const Portfolio = () => (
   <div className="Portfolio">
-    <div className="img">
-      <img src={require('images/naska/1.jpg')}/>
+    <div className="container">
+      <div className="grid">
+        <div className="img">
+          <img src={require('images/naska/1.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/2.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/3.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/4.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/5.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/6.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/7.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/8.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/9.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/10.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/11.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/12.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/13.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/14.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/15.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/16.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/17.jpg')}/>
+        </div>
+        
+        <div className="img">
+          <img src={require('images/naska/18.jpg')}/>
+        </div>
+      </div>
     </div>
-    
-    <div className="img">
-      <img src={require('images/naska/2.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/3.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/4.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/5.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/6.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/7.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/8.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/9.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/10.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/11.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/12.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/13.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/14.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/15.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/16.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/17.jpg')}/>
-    </div>
-    
-    <div className="img">
-      <img src={require('images/naska/18.jpg')}/>
-    </div>
-  </
-  div>
+  </div>
 )
 
 export default Portfolio
