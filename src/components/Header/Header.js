4cd8135ebac673fd441cda96ae77cc4bcbd8965d
ericgrosse <ericgrosse1@gmail.com>
2017-10-28 17:29:31 -0400

    Header refactoring

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,30 +4,26 @@ import 'components/Header/Header.scss'
 
 const Header = () => (
   <div className="Header">
-    <div className="grid">
-      <div className="gutter"/>
+    <div className="container">
+      <div className="grid">
+        <div className="portfolio-container">
+          <Link className="header portfolio-button" to='/portfolio'>Portfolio</Link>
 
-      <div className="portfolio-container">
-        <Link className="header portfolio-button" to='/portfolio'>Portfolio</Link>
+          <div className="portfolio-subsection">
+            <Link className="header portfolio-button" to='/portfolio'>Naska</Link>
+            <Link className="header portfolio-button" to='/portfolio'>Troy</Link>
+          </div>
+        </div>
 
-        <div className="portfolio-subsection">
-          <Link className="header portfolio-button" to='/portfolio'>Naska</Link>
-          <Link className="header portfolio-button" to='/portfolio'>Troy</Link>
+        <div className="about-container">
+          <Link className="header about-button" to='/about'>About</Link>
+        </div>
+
+        <div className="quote-request-container">
+          <Link className="header quote-request-button" to='/contact'>Contact</Link>
         </div>
       </div>
-
-      <div className="about-container">
-        <Link className="header about-button" to='/about'>About</Link>
-      </div>
-
-      <div className="quote-request-container">
-        <Link className="header quote-request-button" to='/contact'>Contact</Link>
-      </div>
-
-      <div className="gutter"/>
     </div>
-
-
   </div>
 )
 
