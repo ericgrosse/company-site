1d9bb1274655742d2975231189f6ed39e67880e4
ericgrosse <ericgrosse1@gmail.com>
2017-10-28 12:56:52 -0400

    Header added support for subsections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,30 @@ import 'components/Header/Header.scss'
 
 const Header = () => (
   <div className="Header">
-    <div className="gutter"/>
+    <div className="grid">
+      <div className="gutter"/>
 
-    <Link className="header portfolio-button" to='/portfolio'>Portfolio</Link>
-    <Link className="header about-button" to='/about'>About</Link>
-    <Link className="header quote-request-button" to='/contact'>Contact</Link>
+      <div className="portfolio-container">
+        <Link className="header portfolio-button" to='/portfolio'>Portfolio</Link>
 
-    <div className="gutter"/>
+        <div className="portfolio-subsection">
+          <Link className="header portfolio-button" to='/portfolio'>Naska</Link>
+          <Link className="header portfolio-button" to='/portfolio'>Troy</Link>
+        </div>
+      </div>
+
+      <div className="about-container">
+        <Link className="header about-button" to='/about'>About</Link>
+      </div>
+
+      <div className="quote-request-container">
+        <Link className="header quote-request-button" to='/contact'>Contact</Link>
+      </div>
+
+      <div className="gutter"/>
+    </div>
+
+
   </div>
 )
 
