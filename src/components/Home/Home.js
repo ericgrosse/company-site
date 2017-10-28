bb5d8f07498f449c1e38a71d309cf68728bdd38b
ericgrosse <ericgrosse1@gmail.com>
2017-10-28 16:47:46 -0400

    homepage significant overhaul

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,12 +35,10 @@ class Home extends Component {
                 type="video/mp4"
               />
             </video>
-
+          </div>
+          
+          <div className="section our-services">
             <div className="sub sub-1">
-              <img src={require('images/logo.png')}/>
-            </div>
-
-            <div className="sub sub-2">
               <h3>Our Services</h3>
               <i className="fa fa-server" aria-hidden="true"/>
               <p>We offer web development services for projects of any scale</p>
@@ -50,7 +48,7 @@ class Home extends Component {
               </Link>
             </div>
 
-            <div className="sub sub-3">
+            <div className="sub sub-2">
               <h3>Our Portfolio</h3>
               <i className="fa fa-folder-open" aria-hidden="true"/>
               <p>We build responsive websites with fluid layouts and elegant animations</p>
@@ -60,7 +58,7 @@ class Home extends Component {
               </Link>
             </div>
 
-            <div className="sub sub-4">
+            <div className="sub sub-3">
               <h3>About Us</h3>
               <i className="fa fa-user-circle" aria-hidden="true"/>
               <p>We are a small team of dedicated web developers and concept artists</p>
@@ -70,7 +68,7 @@ class Home extends Component {
               </Link>
             </div>
           </div>
-          
+
           <div className="section our-expertise">
             <div className="sub sub-1">
               <h3>Our Expertise</h3>
