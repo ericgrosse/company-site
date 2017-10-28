0c6cd709fa41dae732a5ac9029f468a6c2b773a5
ericgrosse <ericgrosse1@gmail.com>
2017-10-28 11:40:17 -0400

    Removed unused routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,8 @@ const Header = () => (
   <div className="Header">
     <div className="gutter"/>
 
-    <Link className="header home-button" to='/'>Home</Link>
     <Link className="header about-button" to='/about'>About</Link>
-    <Link className="header services-button" to='/services'>Services</Link>
     <Link className="header portfolio-button" to='/portfolio'>Portfolio</Link>
-    <Link className="header quote-request-button" to='/quote-request'>Quote Request</Link>
     <Link className="header quote-request-button" to='/contact'>Contact</Link>
 
     <div className="gutter"/>
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from 'components/Home/Home'
 import About from 'components/About'
-import Services from 'components/Services'
 import Portfolio from 'components/Portfolio'
-import QuoteRequest from 'components/QuoteRequest'
 import Contact from 'components/Contact'
 
 const Routes = () => (
@@ -12,9 +10,7 @@ const Routes = () => (
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route path='/about' component={About}/>
-      <Route path='/services' component={Services}/>
       <Route path='/portfolio' component={Portfolio}/>
-      <Route path='/quote-request' component={QuoteRequest}/>
       <Route path='/contact' component={Contact}/>
     </Switch>
   </main>
