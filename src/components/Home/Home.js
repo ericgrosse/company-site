6691895ffd2fd842f164ddda1a644f5d111d2df4
ericgrosse <ericgrosse1@gmail.com>
2017-10-29 09:22:53 -0400

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,85 +36,35 @@ class Home extends Component {
               />
             </video>
           </div>
-
-          <div className="section our-services">
-            <div className="subsection left">
-              <h3>Our Services</h3>
-              <p>We offer web development services for projects of any scale</p>
-              <Link to="/services">What we offer</Link>
-            </div>
-
-            <div className="subsection right">
-              <img src={require('images/naska/1.jpg')}/>
-            </div>
-          </div>
           
           <div className="section our-portfolio">
             <div className="subsection left">
               <h3>Our Portfolio</h3>
               <p>We build responsive websites with fluid layouts and elegant animations</p>
               <Link to="/portfolio">What we've done</Link>
-              <img src={require('images/naska/2.jpg')}/>
+              <img src={'http://via.placeholder.com/1080x720'}/>
             </div>
 
             <div className="subsection right">
               <h3>About Us</h3>
               <p>We are a small team of dedicated web developers and concept artists</p>
               <Link to="/about">What we're about</Link>
-              <img src={require('images/naska/3.jpg')}/>
+              <img src={'http://via.placeholder.com/1080x720'}/>
             </div>
           </div>
-
-          {/*<div className="section our-expertise">
-            <div className="subsection subsection-1">
-              <h3>Our Expertise</h3>
-            </div>
-            
-            <div className="subsection subsection-2">
-              <img src={require('images/react-logo-1000x1000.png')}/>
-            </div>
-            
-            <div className="subsection subsection-3">
-              <img src={require('images/webpack-logo-800x925.png')}/>
-            </div>
-            
-            <div className="subsection subsection-4">
-              <img src={require('images/node-js-logo-1000x612.png')}/>
-            </div>
-
-            <div className="subsection subsection-5">
-              <img src={require('images/html5+css3.png')}/>
-            </div>
-
-            <div className="subsection subsection-6">
-              <img src={require('images/mysql-logo-1024x530.png')}/>
-            </div>
-
-            <div className="subsection subsection-7">
-              <img src={require('images/mongodb-logo-375x375.png')}/>
-            </div>
-
-            <div className="subsection subsection-8">
-              <img src={require('images/github-icon-512x512-2.png')}/>
-            </div>
-
-            <div className="subsection subsection-9">
-              <img src={require('images/circle-ci-logo-400x400.png')}/>
-            </div>
-
-            <div className="subsection subsection-10">
-              <img src={require('images/photoshop-logo-512x512.png')}/>
-            </div>
-          </div>*/}
           
           <div className="section our-office">
-            <div className="subsection subsection-1">
+            <div className="subsection left">
               <h3>Our Office</h3>
               <p>Montreal</p>
               <p>153 ave de la Pointe-Claire</p>
               <p>Pointe-Claire, Quebec</p>
               <p>Canada H9S 4M7</p>
               <p>Tel: 514-951-5198</p>
+            </div>
+
+            <div className="subsection right">
+              <img src={'http://via.placeholder.com/1080x720'}/>
             </div>
           </div>
 
@@ -217,6 +167,60 @@ class Home extends Component {
               </div>
             </div>
           </div>
+
+          {/*<div className="section our-services">
+            <div className="subsection left">
+              <h3>Our Services</h3>
+              <p>We offer web development services for projects of any scale</p>
+              <Link to="/services">What we offer</Link>
+            </div>
+
+            <div className="subsection right">
+              <img src={require('images/naska/1.jpg')}/>
+            </div>
+          </div>*/}
+
+          {/*<div className="section our-expertise">
+            <div className="subsection subsection-1">
+              <h3>Our Expertise</h3>
+            </div>
+            
+            <div className="subsection subsection-2">
+              <img src={require('images/react-logo-1000x1000.png')}/>
+            </div>
+            
+            <div className="subsection subsection-3">
+              <img src={require('images/webpack-logo-800x925.png')}/>
+            </div>
+            
+            <div className="subsection subsection-4">
+              <img src={require('images/node-js-logo-1000x612.png')}/>
+            </div>
+
+            <div className="subsection subsection-5">
+              <img src={require('images/html5+css3.png')}/>
+            </div>
+
+            <div className="subsection subsection-6">
+              <img src={require('images/mysql-logo-1024x530.png')}/>
+            </div>
+
+            <div className="subsection subsection-7">
+              <img src={require('images/mongodb-logo-375x375.png')}/>
+            </div>
+
+            <div className="subsection subsection-8">
+              <img src={require('images/github-icon-512x512-2.png')}/>
+            </div>
+
+            <div className="subsection subsection-9">
+              <img src={require('images/circle-ci-logo-400x400.png')}/>
+            </div>
+
+            <div className="subsection subsection-10">
+              <img src={require('images/photoshop-logo-512x512.png')}/>
+            </div>
+          </div>*/}
         </div>
       </div>
     );
