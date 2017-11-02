a395509a5b8c6bcafa00cd6f51da5dad1bdfecbb
ericgrosse <ericgrosse1@gmail.com>
2017-11-02 17:32:56 -0400

    Added Banner component (skeleton)

---
+++ src/components/Common/Banner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Banner.scss'
+
+export default class Banner extends Component {
+  render() {
+    return (
+      <section className="Banner">
+        Banner
+      </section>
+    )
+  }
+}
