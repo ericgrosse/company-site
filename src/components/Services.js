3d37f2ff9560b0e4b48275f69cc36bdd2429fb8a
ericgrosse <ericgrosse1@gmail.com>
2017-11-04 11:20:01 -0400

    Services added Our Expertise section

--- src/components/Services.js
+++ src/components/Services.js
@@ -71,18 +71,46 @@ class Services extends Component {
           </div>
         </section>
 
-        <section>
+        <section className="our-expertise">
           <div className="grid-container">
             <div className="subsection header">
               <h3>Our Expertise</h3>
             </div>
-
-            <div className="subsection photo">
-              <img className="grid-image effect-right" src={'http://via.placeholder.com/1080x720'}/>
+             
+            <div className="subsection image-container">
+              <img src={require('images/react-logo-1000x1000.png')}/>
+            </div>
+            
+            <div className="subsection image-container">
+              <img src={require('images/webpack-logo-800x925.png')}/>
+            </div>
+            
+            <div className="subsection image-container">
+              <img src={require('images/node-js-logo-1000x612.png')}/>
             </div>
 
-            <div className="subsection content">
-              <p>Suspendisse a est vitae tortor pretium pulvinar. Cras ullamcorper elit enim, vel auctor libero vestibulum fermentum. Praesent a arcu ut nisi tempus tempor. Aliquam et euismod nulla. Praesent euismod condimentum nulla, eu sagittis nisl interdum vitae. Suspendisse libero felis, tempor sit amet tempus sit amet, tincidunt sit amet risus. Nam accumsan blandit molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante libero, scelerisque vitae diam sit amet, pellentesque luctus quam. Aenean in sapien et neque iaculis laoreet. Donec efficitur aliquet sem non imperdiet. Aliquam non egestas orci. Nulla varius rutrum ex in fringilla. Praesent pharetra ullamcorper quam, in ornare erat. Fusce eget laoreet dolor. Aliquam convallis dapibus nisl, ac sodales mauris accumsan eget.</p>
+            <div className="subsection image-container">
+              <img src={require('images/html5+css3.png')}/>
+            </div>
+
+            <div className="subsection image-container">
+              <img src={require('images/mysql-logo-1024x530.png')}/>
+            </div>
+
+            <div className="subsection image-container">
+              <img src={require('images/mongodb-logo-375x375.png')}/>
+            </div>
+
+            <div className="subsection image-container">
+              <img src={require('images/github-icon-512x512-2.png')}/>
+            </div>
+
+            <div className="subsection image-container">
+              <img src={require('images/circle-ci-logo-400x400.png')}/>
+            </div>
+
+            <div className="subsection image-container">
+              <img src={require('images/photoshop-logo-512x512.png')}/>
             </div>
           </div>
         </section>
